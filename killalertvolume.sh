8903baf7d9b83166677983ec6222a3e4bee6731e
Steve Kilduf <redrocketed@gmail.com>
2014-02-23 19:55:22 +0100

    Add more commands for getting volume

--- killalertvolume.sh
+++ killalertvolume.sh
@@ -1,3 +1,5 @@
 ## Kill the annoying terminal bell, or when string is not found
 ## when searching in chrome
 osascript -e 'set volume alert volume 0'
+osascript -e 'output volume of (get volume settings)'
+osascript -e 'get volume settings'
